79047929bce99cf94a621bcb72c78308f5477f8a
Dokoye98 <Dwayneokoye98@gmail.com>
2024-09-04 00:17:10 +0100

    startup

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import profilePic from '../images/profilePic.JPG'
+
+function Homepage(){
+
+
+    return(
+        <div>
+            <h1>Hi my name is D'wayne Okoye-Kachikwu</h1>
+           
+        </div>
+    )
+}
+
+export default Homepage
