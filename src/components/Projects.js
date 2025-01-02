2a8a082fc6b2e7de98d5ed8f264a92d11061f1c1
Dokoye98 <Dwayneokoye98@gmail.com>
2025-01-02 16:27:18 +0000

    More info added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,12 +21,26 @@ function Project(){
     target="_blank"
     rel="noopener noreferrer"
   >
-    <p>iT Troubleshooting Website Link</p>
+    <p>IT Troubleshooting Website Link</p>
   </a>
   <ul>
     <li>
-      This website provides access to tests for various CompTIA certification 
+    This web-based platform offers users access to interactive tests designed to help them prepare for various CompTIA certifications, such as CompTIA A+, Network+, and Security+
     </li>
+  </ul>
+  <p>Key Features</p>
+  <ul>
+    <li>
+    <b>CompTIA certification prep: </b> The website offers tests tailored to CompTIA certification objectives, helping users gauge their readiness for the official exams.
+    </li>
+    <li>
+      <b>User-Friendly Interface: </b> The platform has a simple, intuitive design, ensuring that users can easily navigate through tests and track their progress.
+      </li>
+      <li>
+      <b>GDPR Compliance: </b>User data is securely managed in line with GDPR regulations, ensuring privacy and security.
+    
+      </li>
+     
   </ul>
           <h3>Social media: Wave</h3>
   <a
