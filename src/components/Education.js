3aa6eb473e1b6955fb6fae54c8a5c02740c5b13c
Dokoye98 <Dwayneokoye98@gmail.com>
2025-01-02 14:53:43 +0000

    new education added

--- src/components/Education.js
+++ src/components/Education.js
@@ -22,13 +22,26 @@ function Education() {
       <div className="experience-item">
           <h2>Master of Science in Advanced Computing</h2>
           <h3>Birkbeck University of London</h3>
-          <p className="date">October 2023 - October 2024</p>
+          <p className="date">October 2023 - December 2024</p>
           <ul>
+            <li><h5>Grade</h5></li>
+            <li>Merit</li>
             <li><h5>Dissertation</h5></li>
             <li>A Comprehensive Guide to Compiler Development: A Hybrid Python-Java Approach with Cloud Accessibility, focusing on user experience and accessibility.</li>
             <li><h5>Modules:</h5> Applied Machine Learning, Big Data Analytics, Cloud Computing, Data Science Techniques, Network Security, Natural Language Processing, Software Design, and Programming.</li>
           </ul>
         </div>
+        <div className="experience-item">
+  <h2>Cybersecurity Training</h2>
+  <h3>Netcom Training</h3>
+  <p className="date">December 2024 - August 2025</p>
+  <ul>
+    <li>Training in Microsoft Azure Fundamentals, CompTIA Security+, and CompTIA CySA+.</li>
+    <li>Gained practical skills in cloud security, risk management, and network security fundamentals.</li>
+    <li>Developed expertise in threat detection, incident response, and security operations.</li>
+    <li>Focused on preparing for certifications that are essential for a career in cybersecurity.</li>
+  </ul>
+</div>
 
       
         <div className="experience-item">
